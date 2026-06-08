0403ee1ac52dc9c0bbf00389ba741e5664e6de99
wongani18 <wonganinyirenda18@gmail.com>
2026-06-08 11:07:21 +0200

    feat: added the delete route

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
@@ -28,5 +28,10 @@ export class UsersController {
     @Patch(':id')
     update(@Param('id') id: string, @Body() userUpdate: {}) {
         return {id, ...userUpdate}
+    }
+
+    @Delete(':id')
+    delete(@Param('id') id: string) {
+        return {id}
     }
 }
