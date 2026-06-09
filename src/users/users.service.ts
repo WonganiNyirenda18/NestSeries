123a94b6c8d4f1a495a4911bdaa0cf7e27da6f60
wongani18 <wonganinyirenda18@gmail.com>
2026-06-09 09:47:42 +0200

    feat: added an array of users with their roles

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,37 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    private users = [
+        {
+            "id": 1,
+            "name": "Wongani Nyirenda",
+            "email": "wongani@gmail.com",
+            "role": "INTERN",
+        },
+        {
+            "id": 2,
+            "name": "Lauryn Mwase",
+            "email": "mwase@gmail.com",
+            "role": "ENGINEER",
+        },
+        {
+            "id": 3,
+            "name": "Rita Makanjira",
+            "email": "rita@gmail.com",
+            "role": "ENGINEER",
+        },
+        {
+            "id": 4,
+            "name": "Carlos Muleke",
+            "email": "carlos@gmail.com",
+            "role": "ENGINEER",
+        },
+        {
+            "id": 5,
+            "name": "Astro Chimwala",
+            "email": "astro@gmail.com",
+            "role": "ADMIN",
+        }
+    ]
+}
