c802584ffed613cd2da2996fab8b4afb2dbfa896
wongani18 <wonganinyirenda18@gmail.com>
2026-06-18 10:13:53 +0200

    feat: added a folder employees

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { EmployeesService } from './employees/employees.service';
 
 @Module({
   imports: [UsersModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, EmployeesService],
 })
 export class AppModule {}
