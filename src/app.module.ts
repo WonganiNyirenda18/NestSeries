dbb5eda962cd08e2aca6201a8e92ed8cbbbcb079
wongani18 <wonganinyirenda18@gmail.com>
2026-06-18 10:15:55 +0200

    feat: added an employee folder containing all services

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { EmployeesService } from './employees/employees.service';
+import { EmployeesModule } from './employees/employees.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, EmployeesModule],
   controllers: [AppController],
   providers: [AppService, EmployeesService],
 })
